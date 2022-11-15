d61a225b5fa486bba80242c2be7fd805b517e4cd
blaze <blazejos1230@gmail.com>
2022-11-15 18:09:20 +0100

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import "../css/components/Header.css";
+import Avatar from "@material-ui/core/Avatar";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    background: "#009BE5",
+    color: "#ffffff",
+    height: "100%",
+    width: "100%",
+    padding: theme.spacing(3),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    boxSizing: "border-box"
+  },
+  avatar: {
+    display: "inline-block",
+    width: theme.spacing(6),
+    height: theme.spacing(6)
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    marginBottom: "auto",
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
+const Header = (props) => {
+  const {username ,title ,handleDrawerToggle} = props.data
+  const classes = useStyles();
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Paper classes={{root: classes.paper}} elevation={5}>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          className={classes.menuButton}
+        >
+          <FontAwesomeIcon icon={faBars}/>
+        </IconButton>
+        <div>
+          <h1>{title}</h1>
+        </div>
+        <div className="InnerDiv">
+          <div className="UserDiv">
+            <span>{username}</span>
+            <Avatar src="/Avatar.jpg" sizes="big" classes={{root: classes.avatar}}/>
+          </div>
+          <span>Your wallet: 2547.55zł</span>
+        </div>
+      </Paper>
+    </AppBar>
+  )
+}
+
+export default Header;
